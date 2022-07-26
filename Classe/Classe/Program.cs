dbe95ef3cfc9541afb9c56773420204d3792f8f7
lippesouzza <84047056+lippesouzza@users.noreply.github.com>
2022-07-26 04:12:56 -0300

    Classe 96% concluido

---
+++ Classe/Classe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classe
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+            var Luci = new Gato (4,35,"Preto","Scooby","Dobermann");
+            Cachorro toto = new Cachorro(4, 55, "Marrom","Toto", "Bull Terrier");
+
+            toto.Caminhar();
+            scooby.Auau();
+        }
+    }
+}
---
+++ Classe/Classe/Src/Animais.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe
+{
+    public class Cachorro
+    {        //Caracteristicas//
+        int QntPatas { get; set; }
+        int Idade { get; set; }
+        string Cor { get; set; }
+        string Nome { get; set; }
+        string Raça { get; set; }
+
+        public Cachorro()
+        {
+        }
+
+        public Cachorro(int qdtPatas, int idade, string cor, string nome, string raça)
+        {
+            QntPatas = qdtPatas;
+            this.Idade = idade;
+            this.Cor = cor;
+            Nome = nome;
+            Raça = raça;
+        }
+
+
+
+
+        //Ações//
+        public void Auau()
+        {
+            Console.WriteLine("Auauau!!!");
+        }
+        public void Caminhar()
+        {
+            Console.WriteLine("Estou caminhando");
+        }
+        public void Pular()
+        {
+            Console.WriteLine("Estou pulando");
+        }
+        public void Corre()
+        {
+            Console.WriteLine("Estou correndo");
+        }
+        public override string ToString()
+        {
+            return $"Raça: {Raça} - Nome {Nome} - Cor {Cor} - Idade {Idade} - QntPatas {QntPatas}";
+        }
+
+
+
+    }
+
+}
